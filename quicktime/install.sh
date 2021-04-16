73d621eb937fd3d0abe6142edd62dff49bed3e52
Leo Huber <leo_huber@icloud.com>
2021-04-16 09:19:06 +0200

    cleanup script, added template mechanism

--- quicktime/install.sh
+++ quicktime/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cp quicktime_set_metadata /usr/local/bin
+cp quicktime_set_metadata_all /usr/local/bin
 cp quicktime_generate_metadata /usr/local/bin
 cp quicktime_generate_metadata_all /usr/local/bin
+cp quicktime_cleanup_metadata_all /usr/local/bin
