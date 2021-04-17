ab0e6899317f90a0123313786365afb839e887ef
Leo Huber <leo_huber@icloud.com>
2021-04-17 20:14:59 +0200

    using mediainfo for codec info generation

--- quicktime/install.sh
+++ quicktime/install.sh
@@ -4,4 +4,6 @@ cp quicktime_set_metadata /usr/local/bin
 cp quicktime_set_metadata_all /usr/local/bin
 cp quicktime_generate_metadata /usr/local/bin
 cp quicktime_generate_metadata_all /usr/local/bin
+cp quicktime_generate_codec_info /usr/local/bin
+cp quicktime_generate_codec_info_all /usr/local/bin
 cp quicktime_cleanup_metadata_all /usr/local/bin
