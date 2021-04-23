e155f9d11a1cba67a6d29803166ddfb774a1125b
Leo Huber <leo_huber@icloud.com>
2021-04-23 14:58:35 +0200

    fixed set codec info scripts

--- quicktime/install_scripts.sh
+++ quicktime/install_scripts.sh
@@ -2,6 +2,8 @@
 
 cp quicktime_set_metadata /usr/local/bin
 cp quicktime_set_metadata_all /usr/local/bin
+cp quicktime_set_codec_info /usr/local/bin
+cp quicktime_set_codec_info_all /usr/local/bin
 cp quicktime_generate_metadata /usr/local/bin
 cp quicktime_generate_metadata_all /usr/local/bin
 cp quicktime_generate_codec_info /usr/local/bin
