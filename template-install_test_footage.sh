d41223a839aa73b116da9632a401dadbcbe52710
Leo Huber <leo_huber@icloud.com>
2024-07-04 15:39:28 +0200

    moved first files to python

--- template-install_test_footage.sh
+++ template-install_test_footage.sh
@@ -1,16 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
-# Adapat if needed
-test_footage_mov="/Volumes/Media/video/03_meta_test_footage/**/*"
+import os; import glob
 
-rm -f *.MP4
-rm -f *.mp4
-rm -f *.mov
-rm -f *.MOV
-rm -f *.mts
-rm -f *.MTS
+# Adapt if needed
+test_footage_mov = "/Volumes/Media/video/03_meta_test_footage/**/*"
 
-cp $test_footage_mov ./
+# Find all movie files in the current directory
+files = glob.glob('*.mp4') + \
+    glob.glob('*.MP4') + \
+    glob.glob('*.mov') + \
+    glob.glob('*.MOV') + \
+    glob.glob('*.mts') + \
+    glob.glob('*.MTS')
+
+# Delete each file
+for file in files:
+    os.remove(file)
+
+# Copy files from test_footage_mov to current directory
+for file in glob.glob(test_footage_mov, recursive=True):
+    os.copy(file, "./")
 
 # Remove txt files
-rm -f *.txt
+txt_files = glob.glob('*.txt')
+for file in txt_files:
+    os.remove(file)
