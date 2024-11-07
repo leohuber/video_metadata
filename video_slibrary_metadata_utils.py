8d9e28543aee00d0d309e873a0c919d3975b7800
Leo Huber <leo_huber@icloud.com>
2024-11-07 15:01:32 +0100

    feat: refactor date extraction logic into video_slibrary_metadata_utils for improved code clarity and reuse

--- video_slibrary_metadata_utils.py
+++ video_slibrary_metadata_utils.py
@@ -3,22 +3,22 @@
 import subprocess
 
 def get_date_created_and_identifier(video_file, extension, model):
-    DATE_CREATED = None
-    IDENTIFIER = None
+    date_created = None
+    identifier = None
 
     # Check if we are processing a QuickTime container
     if extension == "mov":
         # Try to get CreationDate
         datec = subprocess.getoutput(f"exiftool -q -q -b -api largefilesupport=1 -CreationDate '{video_file}'")
         if datec:
-            DATE_CREATED = datec
-            IDENTIFIER = DATE_CREATED[:19].replace(' ', '_').replace(':', '')
+            date_created = datec
+            identifier = date_created[:19].replace(' ', '_').replace(':', '')
         else:
             # Try to get CreateDate
             datec = subprocess.getoutput(f"exiftool -q -q -b -api largefilesupport=1 -CreateDate '{video_file}'")
             if datec:
-                DATE_CREATED = datec
-                IDENTIFIER = DATE_CREATED[:19].replace(' ', '_').replace(':', '')
+                date_created = datec
+                identifier = date_created[:19].replace(' ', '_').replace(':', '')
 
     # Check if we are processing an MP4 container
     if extension == "mp4":
@@ -26,13 +26,13 @@ def get_date_created_and_identifier(video_file, extension, model):
             # Get SubSecCreateDate for Canon R6
             datec = subprocess.getoutput(f"exiftool -q -q -b -api largefilesupport=1 -SubSecCreateDate '{video_file}'")
             if datec:
-                DATE_CREATED = datec
-                IDENTIFIER = DATE_CREATED[:19].replace(' ', '_').replace(':', '')
+                date_created = datec
+                identifier = date_created[:19].replace(' ', '_').replace(':', '')
         else:
             # Try to get CreateDate
             datec = subprocess.getoutput(f"exiftool -q -q -b -api largefilesupport=1 -CreateDate '{video_file}'")
             if datec:
-                DATE_CREATED = datec
-                IDENTIFIER = DATE_CREATED[:19].replace(' ', '_').replace(':', '')
+                date_created = datec
+                identifier = date_created[:19].replace(' ', '_').replace(':', '')
 
-    return DATE_CREATED, IDENTIFIER
+    return date_created, identifier
