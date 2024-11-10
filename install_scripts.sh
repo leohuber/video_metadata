f5b459e7d5d18cc3753d1271ef4f1b874edc4aa8
Leo Huber <leo_huber@icloud.com>
2024-11-10 17:06:53 +0100

    feat: streamline installation script by removing unnecessary file copies and adding missing utility script

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 cp video_cleanup_metadata /usr/local/bin
-cp video_generate_codec_info /usr/local/bin
-cp video_generate_date_filename /usr/local/bin
 cp video_generate_metadata /usr/local/bin
-cp video_set_codec_info /usr/local/bin
 cp video_set_metadata /usr/local/bin
-cp video_generate_metadata_template /usr/local/bin
 cp video_slibrary_metadata_utils.py /usr/local/bin
+cp video_slibrary_print_utils.py /usr/local/bin
