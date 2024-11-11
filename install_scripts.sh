ab18772a0b4e4cf2bd21ead145a3865f9704fe67
Leo Huber <leo_huber@icloud.com>
2024-11-11 19:15:29 +0100

    feat: add video_date_util script to installation script

--- install_scripts.sh
+++ install_scripts.sh
@@ -5,3 +5,4 @@ cp video_generate_metadata /usr/local/bin
 cp video_set_metadata /usr/local/bin
 cp video_slibrary_metadata_utils.py /usr/local/bin
 cp video_slibrary_print_utils.py /usr/local/bin
+cp video_date_util /usr/local/bin
