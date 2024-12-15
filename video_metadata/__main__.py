5a4a5226b10385781602cee89ec01453a8d7d69a
Leo Huber <leo_huber@icloud.com>
2024-12-15 20:05:09 +0100

    feat: update package source to editable and remove unused print functions

--- video_metadata/__main__.py
+++ video_metadata/__main__.py
@@ -5,7 +5,7 @@ from video_cleanup_metadata import cleanup_metadata_files
 from video_date_util import print_dates_for_movies
 from video_generate_metadata import generate_metadata_for_files
 from video_set_metadata import set_metadata_for_files
-from lib.video_slibrary_print_utils import print_green, print_red, print_blue
+from lib.video_slibrary_print_utils import print_red
 from lib.video_slibrary_file_utils import expand_path_video
 
 __version__ = "DEVELOPMENT_VERSION"
