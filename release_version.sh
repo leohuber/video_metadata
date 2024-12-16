a110f728291cfd8152d7cace8ede170266b846fb
Leo Huber <leo_huber@icloud.com>
2024-12-16 11:51:01 +0100

    fix: correct version format in pyproject.toml and update release script

--- release_version.sh
+++ release_version.sh
@@ -3,7 +3,7 @@
 VERSION="1.1.0"
 
 # Update the version in the pyproject.toml file
-sed -i '' "s/^version = .*/version = $VERSION/" pyproject.toml
+sed -i '' "s/^version = .*/version = \"$VERSION\"/" pyproject.toml
 
 # Update the version in the __version__.py file
 sed -i '' "1s/.*/__version__ = \"$VERSION\"/" ./video_metadata/__version__.py
